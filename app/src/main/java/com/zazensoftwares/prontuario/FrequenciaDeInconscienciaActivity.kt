b40bc1a0d0d8ed1e41682cbe62e9185d6f06f4d7
Sebastiao Vinícius <sebastianbavini@hotmail.com>
2023-08-03 18:10:31 -0300

    adicionados radio buttons para niveis de inconsciencia

---
+++ app/src/main/java/com/zazensoftwares/prontuario/FrequenciaDeInconscienciaActivity.kt
@@ -0,0 +1,11 @@
+package com.zazensoftwares.prontuario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrequenciaDeInconscienciaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frequencia_de_inconsciencia)
+    }
+}
