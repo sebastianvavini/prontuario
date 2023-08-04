7e3e249dfd268fc03ce3b74a1a1999e1575c5989
Sebastiao Vinícius <sebastianbavini@hotmail.com>
2023-08-04 12:11:34 -0300

    Adicionada navegação no primeiro botao

--- app/src/main/java/com/zazensoftwares/prontuario/BaseActivity.kt
+++ app/src/main/java/com/zazensoftwares/prontuario/BaseActivity.kt
@@ -1,11 +1,37 @@
 package com.zazensoftwares.prontuario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.zazensoftwares.prontuario.databinding.ActivityBaseBinding
 
-class BaseActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity(), View.OnClickListener {
+ private lateinit var binding: ActivityBaseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+        binding= ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonAvancarParaNivelInconsciencia.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+
+        if(v.id==R.id.button_avancar_para_nivel_inconsciencia){
+            validar()
+        }
+
+    }
+
+    fun validar(){
+        avancarParaMensuravel()
+    }
+
+    private fun avancarParaMensuravel(){
+
+        var intencaoFrequencia= Intent (this,FrequenciaDeInconscienciaActivity::class.java)
+        startActivity(intencaoFrequencia)
+        finish()
     }
 }
