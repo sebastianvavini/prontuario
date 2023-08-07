2fe14ae4f7aea5858d295ce7886f8392c85fb444
Sebastiao Vinícius <sebastianbavini@hotmail.com>
2023-08-07 16:44:20 -0300

    decidido separar as tres perguntas basicas para simplificar o layout

---
+++ app/src/main/java/com/zazensoftwares/prontuario/PerguntaBasica1Activity.kt
@@ -0,0 +1,11 @@
+package com.zazensoftwares.prontuario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerguntaBasica1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pergunta_basica1)
+    }
+}
